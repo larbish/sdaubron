2f64a70b074b5becfc1ceb87ddab2f479af1592a
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-08-21 16:56:56 +0200

    fix footer mobile

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -12,26 +12,31 @@
         to="https://www.google.fr/maps/place/1+Rue+des+M%C3%A9sanges,+91400+Orsay/@48.6967101,2.1864111,17z/data=!3m1!4b1!4m6!3m5!1s0x47e5d61d09ae28b1:0x7a9c7ec2169b773f!8m2!3d48.6967066!4d2.188986!16s%2Fg%2F11smz8lpp5?entry=ttu"
         target="_blank"
         icon="i-heroicons-home"
-        label="1 rue des mésanges, 91400, Orsay"
         color="gray"
         variant="ghost"
-      />
+      >
+        <span class="hidden sm:block">1 rue des mésanges, 91400, Orsay</span>
+      </UButton>
       <UButton
         to="tel:0169280765"
         target="_blank"
         icon="i-heroicons-phone"
-        label="01 69 28 07 65"
         color="gray"
         variant="ghost"
-      />
+      >
+        <span class="hidden sm:block">01 69 28 07 65</span>
+      </UButton>
       <UButton
         to="mailto:sandrine.daubron@wanadoo.fr"
         target="_blank"
         icon="i-heroicons-at-symbol-20-solid"
-        label="sandrine.daubron@wanadoo.fr"
         color="gray"
         variant="ghost"
-      />
+      >
+        <span class="hidden sm:block">
+          sandrine.daubron@wanadoo.fr
+        </span>
+      </UButton>
     </template>
   </UFooter>
 </template>
